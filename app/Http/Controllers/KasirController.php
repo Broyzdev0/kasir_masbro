9728a620e9b6bd5dee9ecc5757a4d2d4e35c37b1
Broyzdev0 <broyzdesign240904@gmail.com>
2023-03-20 20:26:44 +0700

    update

--- app/Http/Controllers/KasirController.php
+++ app/Http/Controllers/KasirController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class KasirController extends Controller
 {
     public function index() {
-        return view('kasir.index');
+        return view('login.kasir.index');
     }
 }
--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class OwnerController extends Controller
 {
     public function index() {
-        return view('owner.index');
+        return view('login.owner.index');
     }
 }
